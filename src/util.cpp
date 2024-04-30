7dae10d2f69378bf7cd7d03208c1af02debdaad0
Benchaton <92680749+BenchatonDev@users.noreply.github.com>
2024-04-30 22:04:17 +0200

    Added clamp function
    Allows for compiling with c++ versions prior to c++17
    DevkitPPC uses c++13 so we don't have acces to std::clamp()

--- src/util.cpp
+++ src/util.cpp
@@ -15,3 +15,8 @@ int find_distance(int x1, int y1, int x2, int y2)
 {
     return sqrt(pow(x1 - x2, 2) + pow(y1 - y2, 2));
 }
+
+double clamp(double d, double min, double max) {
+  const double t = d < min ? min : d;
+  return t > max ? max : t;
+}
